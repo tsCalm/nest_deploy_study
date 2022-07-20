6356285174dfb639a75e288db1e75168c777c7ec
tsCalm <dlatkdals92#gmail.com>
2022-07-21 05:28:41 +0900

    test

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'health check - docker test 6';
+    return 'health check - docker test 7';
   }
 }
